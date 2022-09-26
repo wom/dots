ff69914bfb69dc1a71024a226a76827a683ffa4c
Chris Mueller <chmuell@microsoft.com>
2022-09-26 13:16:46 -0400

    Removing indentline specialchars.

--- .config/nvim/lua/configs/indentline.lua
+++ .config/nvim/lua/configs/indentline.lua
@@ -3,8 +3,8 @@ if not require('utils').isModuleAvailable('indent_blankline') then
     return
 end
 vim.opt.list = true
-vim.opt.listchars:append "space:⋅"
-vim.opt.listchars:append "eol:↴"
+-- vim.opt.listchars:append "space:⋅"
+-- vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
     space_char_blankline = " ",
