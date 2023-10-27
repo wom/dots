3a33555575035ab415f241a806592562a63b1653
Chris Mueller <chmuell@microsoft.com>
2023-10-27 15:55:21 -0400

    checkpointing.

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -13,6 +13,7 @@ require("lazy").setup({
         -- import any extras modules here
         { import = "lazyvim.plugins.extras.lang.python" },
         { import = "lazyvim.plugins.extras.coding.copilot" },
+        { import = "lazyvim.plugins.extras.ui.alpha" }, -- Default is now Dashboard, should migrate it.
         -- { import = "lazyvim.plugins.extras.lang.typescript" },
         { import = "lazyvim.plugins.extras.lang.json" },
         -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -6,6 +6,64 @@ return {
             vim.o.timeout = true
             vim.o.timeoutlen = 300
             local wkey = require("which-key")
+            wkey.register({
+                ["<leader>"] = {
+                    -- d = { -- Steal this I think; i hate the f mappings I have.
+                    --   name = "debugging/diff",
+                    --   b = "Debug Breakpoint toggle",
+                    --   c = "Debug Continue",
+                    --   f = "Diffview file",
+                    --   h = "Debug Step out",
+                    --   i = "Debug Inspect selection",
+                    --   j = "Debug Step over",
+                    --   l = "Debug Step in",
+                    --   o = "Debug open float",
+                    --   r = "Debug repl open",
+                    --   s = "Debug Stop",
+                    --   v = "Diffview toggle",
+                    -- },
+                    f = { -- This looks stealable
+                        name = "find/files",
+                        --   a = "File find (all)",
+                        --   b = "Find buffers",
+                        --   ["<C-b>"] = "File browser",
+                        --   ["<C-g>"] = "File grep (select directory)",
+                        --   f = "File find (no . or gitignore)",
+                        --   g = "File grep (all)",
+                        --   G = "File grep (exclude directory)",
+                        --   h = "Find help",
+                        --   s = "Find symbols",
+                        --   S = "Find workspace symbols",
+                        --   t = "Find treesitter",
+                        --   w = "Find word under cursor",
+                    },
+                    o = {
+                        name = "Overseer (Task Runner)",
+                    },
+                    q = {
+                        name = "Quickfix",
+                        q = "Toggle",
+                        --   e = "Edit",
+                    },
+                    t = {
+                        name = "test/terminal",
+                        a = "Test all summary",
+                        d = "Test debug nearest",
+                        f = "Terminal float",
+                        h = "Terminal horizontal",
+                        l = "Test nearest",
+                        o = "Test output",
+                        s = "Test stop",
+                        t = "Test suite run",
+                        v = "Terminal vertical",
+                    },
+                    s = "Search Open Buffers",
+                    S = "Search Workspace ",
+                    w = {
+                        name = "vimwiki",
+                    },
+                },
+            })
         end,
         opts = {},
     },
