3ae5eea3db67d959e5f2bb7eafac33e382fb8a53
Chris Mueller <chmuell@microsoft.com>
2023-10-31 12:56:25 -0400

    fixing copilot.

--- .config/nvim/lua/wom/plugins/copilot.lua
+++ .config/nvim/lua/wom/plugins/copilot.lua
@@ -1,19 +1,33 @@
 return {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-        filetypes = {
-            markdown = true,
-            python = true,
-            yaml = true,
-            lua = true,
-            bash = true,
-            sh = true,
-            zsh = true,
-            help = true,
-        },
-    }
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = "Copilot",
+        build = ":Copilot auth",
+        config = function()
+            require('copilot').setup({
+                suggestion = { enabled = false },
+                panel = { enabled = false },
+                filetypes = {
+                    markdown = true,
+                    python = true,
+                    yaml = true,
+                    lua = true,
+                    bash = true,
+                    sh = true,
+                    go = true,
+                    help = false,
+                    gitcommit = false,
+                    gitrebase = false,
+                    ["."] = false,
+                },
+            })
+        end,
+    },
+    {
+        "zbirenbaum/copilot-cmp",
+        dependencies = { "copilot.lua" },
+        config = function()
+            require("copilot_cmp").setup()
+        end
+    },
 }
