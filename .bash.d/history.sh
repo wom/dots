ebbb17857c888662fea4408e261e2379654629c4
Chris <worminater@gmail.com>
2024-08-18 21:42:29 -0400

    cleaning up mcfly a bit.

--- .bash.d/history.sh
+++ .bash.d/history.sh
@@ -5,12 +5,17 @@ export HISTCONTROL=ignorespace              # leading space hides commands from 
 export HISTFILESIZE=10000                   # increase history file size (default is 500)
 export HISTSIZE=${HISTFILESIZE}             # increase history size (default is 500)
 if command -v mcfly &> /dev/null; then
+    export MCFLY_LIGHT=FALSE
+    export MCFLY_KEY_SCHEME=vim
+    export MCFLY_RESULTS=50
+    export MCFLY_DISABLE_MENU=TRUE
+    export MCFLY_PROMPT="→"
     eval "$(mcfly init bash)"
 elif command -v hstr &> /dev/null; then
     # if this is interactive shell, then bind hstr to Ctrl-r (for Vi mode check doc)
     if [[ $- =~ .*i.* ]]; then bind '"\C-r": "\C-a hstr -- \C-j"'; fi
     # if this is interactive shell, then bind 'kill last command' to Ctrl-x k
     if [[ $- =~ .*i.* ]]; then bind '"\C-xk": "\C-a hstr -k \C-j"'; fi
+    # ensure synchronization between bash memory and history file - buggy
+    # export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"
 fi
-# ensure synchronization between bash memory and history file
-# export PROMPT_COMMAND="history -a; history -n; ${PROMPT_COMMAND}"
